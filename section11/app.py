c14a2ef33035b6514bd0799e3627db9030dbc0a4
Jose Salvatierra <jslvtr@gmail.com>
2018-05-05 14:58:40 +0100

    Add propagate exceptions.

--- section11/app.py
+++ section11/app.py
@@ -10,6 +10,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
 """
