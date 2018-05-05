40ce6aad56b4f7a5b22693f062e92b672bbbf63d
Jose Salvatierra <jslvtr@gmail.com>
2018-05-05 15:28:48 +0100

    Remove .json() from user model.

--- section11/models/user.py
+++ section11/models/user.py
@@ -12,12 +12,6 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
-    def json(self):
-        return {
-            'id': self.id,
-            'username': self.username
-        }
-
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
