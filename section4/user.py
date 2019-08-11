be978aef340ded1d4338c40d3d59b96808ae976f
Russell Romney <russdude7@gmail.com>
2019-08-11 13:42:13 -0500

    change id to _id

--- section4/user.py
+++ section4/user.py
@@ -1,5 +1,5 @@
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
+    def __init__(self, _id, username, password):
+        self.id = _id
         self.username = username
         self.password = password
