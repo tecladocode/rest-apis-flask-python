fbad798073388889046b5ef5750609780cdc5967
Jose Salvatierra <jslvtr@gmail.com>
2022-03-09 16:12:23 +0000

    Add marshmallow schemas for each model, and use marshmallow in the Item resource for serialization and deserialization.

--- project/using-flask-restx/models/store.py
+++ project/using-flask-restx/models/store.py
@@ -2,18 +2,18 @@ from db import db
 
 
 class StoreModel(db.Model):
-    __tablename__ = 'stores'
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
 
-    items = db.relationship('ItemModel', back_populates="store", lazy='dynamic')
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
 
     def json(self):
         return {
-            'id': self.id,
-            'name': self.name,
-            'items': [item.json() for item in self.items.all()]
+            "id": self.id,
+            "name": self.name,
+            "items": [item.json() for item in self.items.all()],
         }
 
     @classmethod
