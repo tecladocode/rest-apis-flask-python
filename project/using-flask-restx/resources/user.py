eb112b7798c34ae32b13b5ab16d2a0cdad485116
Jose Salvatierra <jslvtr@gmail.com>
2022-03-09 16:40:03 +0000

    Remove unused reqparse import from all resources.

--- project/using-flask-restx/resources/user.py
+++ project/using-flask-restx/resources/user.py
@@ -1,5 +1,5 @@
 from flask import request
-from flask_restx import Resource, reqparse
+from flask_restx import Resource
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
