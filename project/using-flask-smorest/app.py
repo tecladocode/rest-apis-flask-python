ea91a0e864036713741e67ead4b1f1652f211429
Jose Salvatierra <jslvtr@gmail.com>
2022-05-29 16:11:46 +0100

    📦️ (OpenAPI) Update OpenAPI version in projects to 3.0.3

--- project/using-flask-smorest/app.py
+++ project/using-flask-smorest/app.py
@@ -14,7 +14,7 @@ from resources.tag import blp as TagBlueprint
 app = Flask(__name__)
 app.config["API_TITLE"] = "Stores REST API"
 app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.2"
+app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
