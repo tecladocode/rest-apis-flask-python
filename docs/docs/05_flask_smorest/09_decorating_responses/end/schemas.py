06a889e2e4df1bf4878ffac811b6b4f705027722
Luka Milivojević <milivojevicl350@gmail.com>
2022-11-01 19:20:51 +0100

    fixes from linear issue

--- docs/docs/05_flask_smorest/09_decorating_responses/end/schemas.py
+++ docs/docs/05_flask_smorest/09_decorating_responses/end/schemas.py
@@ -5,7 +5,7 @@ class ItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Int(required=True)
+    store_id = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
