9674a8fde7c05c2be757fc7cd9beb90274d73ffc
Justin Reeves <mail.to.justin@gmail.com>
2023-01-12 10:12:10 -0500

    patch(models): change Tag name column to unique = False in 39 files

--- docs/docs/12_task_queues_emails/04_populate_rq_task_queue/start/models/tag.py
+++ docs/docs/12_task_queues_emails/04_populate_rq_task_queue/start/models/tag.py
@@ -5,7 +5,7 @@ class TagModel(db.Model):
     __tablename__ = "tags"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
 
     store = db.relationship("StoreModel", back_populates="tags")
