19a1442e99562a324ca2af29653900e9d88079e0
fridavbg <fperssontech@gmail.com>
2023-09-24 18:51:35 +0200

    Section 3 finished

--- project/01-first-rest-api/app.py
+++ project/01-first-rest-api/app.py
@@ -14,6 +14,9 @@ stores = [
     }
 ]
 
+STORE_404_ERROR = "Store not found"
+
+
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
@@ -35,7 +38,7 @@ def create_item(name):
             new_item = {"name": request_data["name"], "price": request_data["price"]}
             store["items"].append(new_item)
             return new_item, 201
-    return {"message": "Store not found"}, 404
+    return {"message": STORE_404_ERROR}, 404
 
 
 @app.get("/store/<string:name>")
@@ -43,7 +46,7 @@ def get_store(name):
     for store in stores:
         if store["name"] == name:
             return store
-    return {"message": "Store not found"}, 404
+    return {"message": STORE_404_ERROR}, 404
 
 
 @app.get("/store/<string:name>/item")
@@ -51,4 +54,4 @@ def get_item_in_store(name):
     for store in stores:
         if store["name"] == name:
             return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+    return {"message": STORE_404_ERROR}, 404
